07b26bb305020041e51dc265678c949fbe4f8f53
toshi0383 <t.suzuki326@gmail.com>
2019-02-24 20:14:52 +0900

    Improve Keymaps

--- keyboards/zinc/keymaps/monks/keymap.c
+++ keyboards/zinc/keymaps/monks/keymap.c
@@ -42,20 +42,20 @@ enum macro_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = { \
   /* Qwerty
    * ,-----------------------------------------.             ,-----------------------------------------.
-   * | Esc  |   Q  |   W  |   E  |   R  |   T  |             |   Y  |   U  |   I  |   O  |   P  | Dele |
+   * | Esc  |   Q  |   W  |   E  |   R  |   T  |             |   Y  |   U  |   I  |   O  |   P  |  =   |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
    * | Ctrl |   A  |   S  |   D  |   F  |   G  |             |   H  |   J  |   K  |   L  |   ;  |  '   |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
    * | Shift|   Z  |   X  |   C  |   V  |   B  |             |   N  |   M  |   ,  |   .  |  /   |Enter |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * | Cmd  | Alt  |Ctl _ |   =  |Space |LOWER |             | RAISE| Tab  | Left | Down |  Up  |Right |
+   * | Cmd  | Alt  |Ctl _ |      | Dele |LOWER |             | RAISE|Space | Tab  |      | Cmd  |Shift |
    * `-----------------------------------------'             `-----------------------------------------'
    */
 [_QWERTY] = LAYOUT_ortho_4x12(
-      KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL, \
+      KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQUAL, \
       KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
-      KC_LGUI,  KC_LALT,  TS_NXT_LNG, KC_EQUAL, KC_SPC, LOWER,                RAISE,  KC_TAB, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT \
+      KC_LGUI,  KC_LALT,  TS_NXT_LNG, _______, KC_DEL, LOWER,                RAISE,  KC_SPC, KC_TAB, _______, KC_LGUI, KC_RSFT \
       ),
 
   /* Lower
