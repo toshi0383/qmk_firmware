2ebc8f9da14e20fa48046ca14d3f4104e5ed45d4
toshi0383 <t.suzuki326@gmail.com>
2019-02-24 20:33:25 +0900

    Add CmdCtl and SCMD

--- keyboards/zinc/keymaps/monks/keymap.c
+++ keyboards/zinc/keymaps/monks/keymap.c
@@ -28,7 +28,9 @@ enum custom_keycodes {
   TS_NXT_LNG,
   TS_NXT_WNDW,
   TS_PRV_WNDW,
-  TS_CMD_V
+  TS_CMD_V,
+  TS_CMD_CTL,
+  TS_SCMD
 };
 
 enum macro_keycodes {
@@ -48,14 +50,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = { \
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
    * | Shift|   Z  |   X  |   C  |   V  |   B  |             |   N  |   M  |   ,  |   .  |  /   |Enter |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * | Cmd  | Alt  |Ctl _ |      | Dele |LOWER |             | RAISE|Space | Tab  |      | Cmd  |Shift |
+   * | Cmd  | Alt  |Ctl _ |CmdCtl| Dele |LOWER |             | RAISE|Space | Tab  |      | SCMD |Shift |
    * `-----------------------------------------'             `-----------------------------------------'
    */
 [_QWERTY] = LAYOUT_ortho_4x12(
-      KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQUAL, \
-      KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
-      KC_LGUI,  KC_LALT,  TS_NXT_LNG, _______, KC_DEL, LOWER,                RAISE,  KC_SPC, KC_TAB, _______, KC_LGUI, KC_RSFT \
+      KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,           KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQUAL, \
+      KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,           KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,           KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
+      KC_LGUI, KC_LALT, TS_NXT_LNG, TS_CMD_CTL, KC_DEL, LOWER,     RAISE,  KC_SPC, KC_TAB, TS_SCMD, KC_LGUI, KC_RSFT \
       ),
 
   /* Lower
@@ -136,6 +138,24 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case TS_CMD_V:
       // Cmd + V
       SEND_STRING(SS_LGUI("V"));
+      return false;
+      break;
+    case TS_SCMD:
+      // Shift + Cmd
+      if (record->event.pressed) {
+          SEND_STRING(SS_DOWN(X_LGUI) SS_DOWN(X_LSHIFT));
+      } else {
+          SEND_STRING(SS_UP(X_LGUI) SS_UP(X_LSHIFT));
+      }
+      return false;
+      break;
+    case TS_CMD_CTL:
+      // Cmd + Ctl
+      if (record->event.pressed) {
+          SEND_STRING(SS_DOWN(X_LGUI) SS_DOWN(X_LCTRL));
+      } else {
+          SEND_STRING(SS_UP(X_LGUI) SS_UP(X_LCTRL));
+      }
       return false;
       break;
     case QWERTY:
